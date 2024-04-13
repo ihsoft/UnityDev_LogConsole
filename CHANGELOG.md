276c70181488d7d60fb04a02da5097d5a3e70146
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2024-04-12 23:04:50 -0700

    Start Timberborn v1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,4 @@
+# Timberborn LogConsole, v1.1 (started on 4/12/2024):
+
 # UnityDev_LogConsole v1.0 (May 4th, 2023):
 * Initial version.
--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UnityDev LogConsole")]
+[assembly: AssemblyTitle("UnityDev LogConsole for Timberborn")]
 [assembly: AssemblyDescription("Convinient in-game logconsle for Unity")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("IHSoft")]
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.*")]
